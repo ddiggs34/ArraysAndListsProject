e8a34a346b55b37026d26e1c4fda2fd84aac37c0
David Diggs <ddiggs34@gmail.com>
2023-10-06 12:51:16 -0400

    complete

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Tracing;
+using System.Linq;
 
 namespace ArraysAndLists
 {
@@ -9,24 +13,108 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            var numbers = new int[] { 0, 1,2,3,4,5,6,7,8,9,10};//can also just go in [] - practicing though
 
-            /* Create two Lists of type int.
+            
+            /* Create two Lists of type int. - 
              * Name one List "evens"
+             * 
+             *
              * Name the other List "odds"
              */
 
-            /* Using either a foreach or for loop,
+            var evens = new List<int>(); //LIST OF EVENS - DONE
+            var odds = new List<int>(); // LIST OF ODD - DONE
+
+            /* Using either a foreach or for loop, -- DONE
              * nest an if statement to check to see
              *  if a number is even or odd.
              * Then add those numbers to either the evens List
              * or the odds List
              */
 
-            /* Now using foreach or for loops,
-             * display each List of even and odd numbers
-             *
-             * Try to be creative in your display
-             */
+
+            foreach (var num in numbers)
+            {
+                if (num > 0 && num % 2 == 0 )
+                {
+                    evens.Add(num);
+                }
+
+                else
+                    odds.Add(num);               
+                
+            }
+
+
+
+            foreach (var number in evens)
+
+            { Console.WriteLine($"{number} is even");
+            
+            }
+
+            foreach (var number in odds)
+
+            { Console.WriteLine($"{number} is odd"); 
+            }
+            
+
+
+
+
+            for (int num = 0; num < numbers.Length; num++)
+            {
+                numbers[num] = (numbers[num] += num);
+
+                /* Now using foreach or for loops,
+            * display each List of even and odd numbers
+            
+
+                
+            * Try to be creative in your display
+            */
+                
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
         }
-    }
-}
+
+
+
+
+
+                    
+
+
+
+                
+
+            }
+
+
+        }
+
+            
+
+
+            
+
+            
+            
+
+           
+        
+    
+
